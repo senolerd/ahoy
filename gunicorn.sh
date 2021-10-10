987a1eb1cebaef025b3dc97190eb498e24b8bfaf
Senol E <whereimdb@gmail.com>
2021-10-09 20:44:55 -0500

    update server initlator (gunicorn)

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,16 +1,16 @@
-AHOY_PORT=4444
-AHOY_HOST=192.168.1.68
-
 
 ###################################################
 
+
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+AHOY_PORT=4444
+AHOY_HOST=$(python3 -c "import docker;docker_client=docker.from_env();print(docker_client.info()['Swarm']['NodeAddr'])")
+
 export AHOY_PORT=$AHOY_PORT
 export AHOY_HOST=$AHOY_HOST
-
-
-echo $AHOY_HOST $AHOY_PORT
-
-
-source venv/bin/activate
 
 gunicorn -w 12 --timeout 180 -b $AHOY_HOST:$AHOY_PORT ahoy:app --reload --log-level INFO
