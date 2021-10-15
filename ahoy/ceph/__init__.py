c35d11d4110266196112c3c8d92b06f87f0f1089
Senol E <whereimdb@gmail.com>
2021-10-14 21:42:01 -0500

    lsblk in json format

--- ahoy/ceph/__init__.py
+++ ahoy/ceph/__init__.py
@@ -36,80 +36,87 @@ def get_node_disks():
 
 @ceph_bp.route('/disks', methods=['GET','POST'])
 def disks():
-    print("Disk called")
 
-    cat_partition_cmd = ["cat","/proc/partitions"]
-    cat_mounts_cmd = ["cat","/proc/mounts"]
-    dmsetup_table_cmd = ["dmsetup","table"]
-    dmsetup_ls_cmd = ["dmsetup","ls"]
+    # cat_partition_cmd = ["cat","/proc/partitions"]
+    # cat_mounts_cmd = ["cat","/proc/mounts"]
+    # dmsetup_table_cmd = ["dmsetup","table"]
+    # dmsetup_ls_cmd = ["dmsetup","ls"]
+    # fdisk_l_cmd = ["fdisk","-l"]
+    lsblk_j_cmd = ["lsblk","-J"]
+
    
-    partitions = subprocess.check_output(cat_partition_cmd).decode().split("\n")[2:-1]
-    mounts = [mount.split(' ')[0] for mount in subprocess.check_output(cat_mounts_cmd).decode().split('\n')]
-
-    lvm_disks = []
-
-    def isUsable(major, minor, blocks, name):
-
-        # Right of the bat ignored disks based on prefix of the disk's names
-        # ignored_disks_suffix = ['mapper', "dm-"]
-
-        # for ignored_disk in ignored_disks_suffix:
-        #     if str(name).startswith(ignored_disk):
-        #         # raise Exception
-        #         return {'usable': False, 'reason':'inIgnoredList'}
-
-        # Loop device filtering
-        if str(name).startswith("loop"):
-            # raise Exception
-            return {'usable': False, 'reason':'loop_device'}
-
-        # Loop device filtering
-        if str(name).startswith("dm-"):
-            # raise Exception
-            return {'usable': False, 'reason':'dm-virt_device'}
-
-        # Mounted disks filtering
-        if f"/dev/{name}" in mounts:
-            return {'usable': False, 'reason':'mounted'}
+    # print(subprocess.check_output(lsblk_j_cmd).decode())
 
 
 
-        # Deceting LVM partitios
-        dmsetup_table  = subprocess.check_output(dmsetup_table_cmd)
+    # partitions = subprocess.check_output(cat_partition_cmd).decode().split("\n")[2:-1]
+    # mounts = [mount.split(' ')[0] for mount in subprocess.check_output(cat_mounts_cmd).decode().split('\n')]
 
-        [ lvm_disks.append(dm_item.split(' ')[4]) for dm_item in dmsetup_table.decode().splitlines() if dm_item.split(' ')[4] not in lvm_disks ]  
+    # lvm_disks = []
+
+    # def isUsable(major, minor, blocks, name):
+
+    #     # Right of the bat ignored disks based on prefix of the disk's names
+    #     # ignored_disks_suffix = ['mapper', "dm-"]
+
+    #     # for ignored_disk in ignored_disks_suffix:
+    #     #     if str(name).startswith(ignored_disk):
+    #     #         # raise Exception
+    #     #         return {'usable': False, 'reason':'inIgnoredList'}
+
+    #     # Loop device filtering
+    #     if str(name).startswith("loop"):
+    #         # raise Exception
+    #         return {'usable': False, 'reason':'loop_device'}
+
+
+    #     # Device Mapper device filtering
+    #     if str(name).startswith("dm-"):
+    #         # raise Exception
+    #         return {'usable': False, 'reason':'dm-virt_device'}
+
+
+    #     # Mounted disks filtering
+    #     if f"/dev/{name}" in mounts:
+    #         return {'usable': False, 'reason':'mounted'}
+
+
+    #     # Deceting LVM partitios
+    #     dmsetup_table  = subprocess.check_output(dmsetup_table_cmd)
+
+    #     [ lvm_disks.append(dm_item.split(' ')[4]) for dm_item in dmsetup_table.decode().splitlines() if dm_item.split(' ')[4] not in lvm_disks ]  
         
-        if f"{major}:{minor}" in lvm_disks:
-                return {'usable': False, 'reason':'dm-block_device'}
+    #     if f"{major}:{minor}" in lvm_disks:
+    #             return {'usable': False, 'reason':'dm-block_device'}
 
 
 
-        return {'usable': True, 'reason': ""}
+    #     return {'usable': True, 'reason': ""}
     
 
 
 
-    disks = {}
-    disk_name = ""
+    # disks = {}
+    # disk_name = ""
 
-    for partition in partitions:
-        major, minor, blocks, name = partition.split()
+    # for partition in partitions:
+    #     major, minor, blocks, name = partition.split()
 
-        try:
-            usability = isUsable(major, minor, blocks, name)
+    #     try:
+    #         usability = isUsable(major, minor, blocks, name)
 
-            if int(minor) == 0:
-                disks[name] = {'major': major, 'minor': minor, 'blocks': blocks, 'partitions':[]}
-                disk_name = name
+    #         if int(minor) == 0:
+    #             disks[name] = {'major': major, 'minor': minor, 'blocks': blocks, 'partitions':[]}
+    #             disk_name = name
             
-            if int(minor) > 0:
-                disk_part = { name:{'major': major, 'minor': minor, 'blocks': blocks, 'usable': usability['usable'], 'reason': usability['reason']} }
-                disks[disk_name]['partitions'].append(disk_part)
+    #         if int(minor) > 0:
+    #             disk_part = { name:{'major': major, 'minor': minor, 'blocks': blocks, 'usable': usability['usable'], 'reason': usability['reason']} }
+    #             disks[disk_name]['partitions'].append(disk_part)
 
-        except:
-            pass
+    #     except:
+    #         pass
             
 
     # print(disks)
-    return jsonify(disks)
+    return subprocess.check_output(lsblk_j_cmd).decode()
 
