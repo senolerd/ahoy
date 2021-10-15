ae5af7d43085a6a325644923df982f50c09bd6d8
Senol E <whereimdb@gmail.com>
2021-10-14 19:33:14 -0500

    seperated filtering

--- ahoy/ceph/__init__.py
+++ ahoy/ceph/__init__.py
@@ -27,47 +27,67 @@ def ceph_nodes():
 
 @ceph_bp.route('/getdisks', methods=['POST'])
 def get_node_disks():
-    data = json.loads(request.data.decode())
-    requested_addr = str(data['ManagerStatus']['Addr']).split(":")[0] if 'ManagerStatus' in data else data['Status']['Addr']
+
+    node_data = json.loads(request.data.decode())
+    requested_addr = str(node_data['ManagerStatus']['Addr']).split(":")[0] if 'ManagerStatus' in node_data else node_data['Status']['Addr']
     requested_addr_disks_url = f"{current_app.config['AHOY_PROTO']}://{requested_addr}:{current_app.config['AHOY_PORT']}/ceph/disks"
     return json.loads(urllib3.PoolManager().request('GET' ,requested_addr_disks_url).data.decode())
     
 
 @ceph_bp.route('/disks', methods=['GET','POST'])
 def disks():
+    print("Disk called")
 
     cat_partition_cmd = ["cat","/proc/partitions"]
     cat_mounts_cmd = ["cat","/proc/mounts"]
     dmsetup_table_cmd = ["dmsetup","table"]
     dmsetup_ls_cmd = ["dmsetup","ls"]
-    lvm_disks = []
-
-
-
-
-    def isIgnored(major, minor, blocks, name):
-
-        # Right of the bat ignored disks based on prefix of the disk's names
-        ignored_disks_suffix = ['loop', 'mapper', "dm-"]
-
-        for ignored_disk in ignored_disks_suffix:
-            if str(name).startswith(ignored_disk):
-                raise Exception
-    
-        # Deceting LVM partitios
-        dmsetup_table  = subprocess.check_output(dmsetup_table_cmd)
-        [ lvm_disks.append(dm_item.split(' ')[4]) for dm_item in dmsetup_table.decode().splitlines() if dm_item.split(' ')[4] not in lvm_disks ]  
-        if f"{major}:{minor}" in lvm_disks:
-            raise Exception
-
-
-
-        print("LVM Disks: ", lvm_disks)
-
-                
-
+   
     partitions = subprocess.check_output(cat_partition_cmd).decode().split("\n")[2:-1]
     mounts = [mount.split(' ')[0] for mount in subprocess.check_output(cat_mounts_cmd).decode().split('\n')]
+
+    lvm_disks = []
+
+    def isUsable(major, minor, blocks, name):
+
+        # Right of the bat ignored disks based on prefix of the disk's names
+        # ignored_disks_suffix = ['mapper', "dm-"]
+
+        # for ignored_disk in ignored_disks_suffix:
+        #     if str(name).startswith(ignored_disk):
+        #         # raise Exception
+        #         return {'usable': False, 'reason':'inIgnoredList'}
+
+        # Loop device filtering
+        if str(name).startswith("loop"):
+            # raise Exception
+            return {'usable': False, 'reason':'loop_device'}
+
+        # Loop device filtering
+        if str(name).startswith("dm-"):
+            # raise Exception
+            return {'usable': False, 'reason':'dm-virt_device'}
+
+        # Mounted disks filtering
+        if f"/dev/{name}" in mounts:
+            return {'usable': False, 'reason':'mounted'}
+
+
+
+        # Deceting LVM partitios
+        dmsetup_table  = subprocess.check_output(dmsetup_table_cmd)
+
+        [ lvm_disks.append(dm_item.split(' ')[4]) for dm_item in dmsetup_table.decode().splitlines() if dm_item.split(' ')[4] not in lvm_disks ]  
+        
+        if f"{major}:{minor}" in lvm_disks:
+                return {'usable': False, 'reason':'dm-block_device'}
+
+
+
+        return {'usable': True, 'reason': ""}
+    
+
+
 
     disks = {}
     disk_name = ""
@@ -76,15 +96,16 @@ def disks():
         major, minor, blocks, name = partition.split()
 
         try:
-            isIgnored(major, minor, blocks, name)
+            usability = isUsable(major, minor, blocks, name)
 
             if int(minor) == 0:
                 disks[name] = {'major': major, 'minor': minor, 'blocks': blocks, 'partitions':[]}
                 disk_name = name
             
             if int(minor) > 0:
-                disk_part = { name:{'major': major, 'minor': minor, 'blocks': blocks, 'usable': True if f'/dev/{name}' in mounts else False} }
+                disk_part = { name:{'major': major, 'minor': minor, 'blocks': blocks, 'usable': usability['usable'], 'reason': usability['reason']} }
                 disks[disk_name]['partitions'].append(disk_part)
+
         except:
             pass
             
